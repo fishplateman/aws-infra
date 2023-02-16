34b92ab350276133169658ff2b08e7e17833fc9c
“fishplateman” <android9s@icloud.com>
2023-02-16 15:33:44 -0500

    update

---
+++ verison/version.tf
@@ -0,0 +1,10 @@
+terraform{
+    required_providers{
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.0 , < 5.0"
+        }
+    }
+
+    required_version = ">= 1.2.0"
+}
