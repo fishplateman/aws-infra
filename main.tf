6ee0dd5224b09fa0f560467f55c31aeaa02d2f0f
“fishplateman” <android9s@icloud.com>
2023-04-14 01:23:47 -0400

    update

--- main.tf
+++ main.tf
@@ -206,12 +206,12 @@ resource "aws_security_group" "lb_sg" {
   vpc_id = aws_vpc.mainvpc.id
 
   # 等下注释了
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    from_port   = 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
   ingress {
     from_port   = 443
